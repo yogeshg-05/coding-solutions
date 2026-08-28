e87756ec3f33f91b4fba34ed86464905f560b70b
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-28 18:51:13 +0530

    ✅ Java If-Else | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-28

--- hackerrank/medium/java-if-else/solution.java
+++ hackerrank/medium/java-if-else/solution.java
@@ -2,16 +2,20 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+        int n = scanner.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else if (n >= 2 && n <= 5) {
+            System.out.println("Not Weird");
+        } else if (n >= 6 && n <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
 
-        scan.close();
+        scanner.close();
     }
 }
