da179881af258245a0748c86cdf587324585e272
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-28 18:49:46 +0530

    ✅ Java Stdin and Stdout I | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-28

--- hackerrank/medium/java-stdin-and-stdout-1/solution.java
+++ hackerrank/medium/java-stdin-and-stdout-1/solution.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        scan.close();
     }
 }
