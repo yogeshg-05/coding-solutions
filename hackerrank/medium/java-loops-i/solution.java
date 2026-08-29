e918fe6ec01bc2eaea950cb19b46e47ab8764307
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-29 20:30:17 +0530

    ✅ Java Loops I | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

---
+++ hackerrank/medium/java-loops-i/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        bufferedReader.close();
+    }
+}
