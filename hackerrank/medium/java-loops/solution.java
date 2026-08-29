64883e0441525bb7db03e6bc7afe1346c5b9a5a8
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-29 20:31:58 +0530

    ✅ Java Loops II | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

--- hackerrank/medium/java-loops/solution.java
+++ hackerrank/medium/java-loops/solution.java
@@ -1,16 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(N + " x " + i + " = " + (N * i));
+            int sum = a;
+
+            for (int j = 0; j < n; j++) {
+                sum += (int)Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            }
+
+            System.out.println();
         }
 
-        bufferedReader.close();
+        in.close();
     }
 }
