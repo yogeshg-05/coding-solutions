4e7767735558f8782b3e768adc6a8127d0978041
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-08-31 17:39:22 +0530

    ✅ Java Static Initializer Block | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ hackerrank/medium/java-static-initializer-block/solution.java
@@ -0,0 +1,16 @@
+
+
+static int B;
+static int H;
+static boolean flag = true;
+
+static {
+    Scanner sc = new Scanner(System.in);
+    B = sc.nextInt();
+    H = sc.nextInt();
+
+    if (B <= 0 || H <= 0) {
+        flag = false;
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+}
