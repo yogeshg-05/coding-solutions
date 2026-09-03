4ae4edfdabdf22290822a140a8e29138ab21d9c5
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-09-03 23:21:02 +0530

    ✅ Java Date and Time | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

--- hackerrank/medium/java-date-and-time/solution.java
+++ hackerrank/medium/java-date-and-time/solution.java
@@ -1,4 +1,28 @@
 
 
-   String s = Integer.toString(n);
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+cal.set(year, month - 1, day);
+
+String[] days = {
+    "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY",
+    "THURSDAY", "FRIDAY", "SATURDAY"
+};
+
+return days[cal.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+
+}
 
