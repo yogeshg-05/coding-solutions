8afbf6e7237673b952ff3ba97af6666d34090e0a
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-09-06 15:06:37 +0530

    ✅ Inheritance Introduction | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-06

--- hackerrank/medium/inheritance-introduction/solution.cpp
+++ hackerrank/medium/inheritance-introduction/solution.cpp
@@ -1,16 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
 
+class Triangle {
+public:
+    void triangle() {
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle {
+public:
+    void isosceles() {
+        cout << "I am an isosceles triangle\n";
+    }
+
+    void description() {
+        cout << "In an isosceles triangle two sides are equal\n";
+    }
+};
+
 int main() {
-    string a, b;
-    cin >> a >> b;
-
-    cout << a.length() << " " << b.length() << endl;
-    cout << a + b << endl;
-
-    swap(a[0], b[0]);
-    cout << a << " " << b << endl;
-
+    Isosceles isc;
+    isc.isosceles();
+    isc.description();
+    isc.triangle();
     return 0;
 }
