12fa073997974508aee98a74fd99596b6f9cec38
Yogesh Gate <mr.yogeshg05@gmail.com>
2026-09-21 23:34:02 +0530

    ✅ Find the Runner-Up Score! | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
+++ hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
@@ -1,13 +1,8 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
     n = int(input())
+    arr = list(map(int, input().split()))
 
-    result = [[i, j, k]
-              for i in range(x + 1)
-              for j in range(y + 1)
-              for k in range(z + 1)
-              if i + j + k != n]
+    unique = list(set(arr))
+    unique.sort()
 
-    print(result)
+    print(unique[-2])
